57948f7588e504e23a538fce574114334e4a2c2b
JonJonesBR <68039931+JonJonesBR@users.noreply.github.com>
2020-07-08 22:53:00 -0300

    Add files via upload

---
+++ show_do_milhao.py
@@ -0,0 +1,540 @@
+def cls(): print('\n'*100)
+
+import time 
+string = "\n>>>BEM VINDO AO SHOW DO MILHÃO<<<\n" 
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+nome=input ("\nEscreva seu nome e aperte Enter: \n\n")
+
+print ("\nOlá",nome, "!\n")    
+
+import time 
+string = "\nNosso jogo começa com 5 perguntas fáceis, seguidas por 5 medianas e 5 difíceis\n" 
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\nE acertando todas as perguntas, você chegará à pergunta de 1 milhão de reais!!!\n" 
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\nBoa sorte!\n" 
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\nAperte Enter para começar\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+comecar=input ()
+
+cls()
+
+import time 
+string = "\nPRIMEIRA RODADA\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n*Ppergunta nº 1: Qual a capital do Brasil?\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n>>Escolha uma alternativa: <<\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+print ("\n1) Montevidéu\n")
+print ("\n2) Caracas\n")
+print ("\n3) Brasília\n")
+print ("\n4) Lima\n")
+
+resposta1=int(input("\nQual é a resposta certa?\n\n"))
+
+if resposta1==3:
+    print ("\nParabéns, você acertou!!!\n")
+
+else:
+    print ("\nQue pena! Você perdeu!\n")
+    exit()
+    
+proxima_pergunta=input ("\nAperte Enter para próxima pergunta\n")
+
+cls()
+
+import time 
+string = "\n*Pergunta n° 2: Qual é o nome dado ao estado da água em forma de gelo?\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n>>Escolha uma alternativa: <<\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+print ("\n1) Líquido\n")
+print ("\n2) Sólido\n")
+print ("\n3) Gasoso\n")
+print ("\n4) Vaporoso\n")
+
+resposta2=int(input("\nQual é a resposta certa?\n\n"))
+
+if resposta2==2:
+    print ("\nParabéns, você acertou!!!\n")
+
+else:
+    print ("\nQue pena! Você perdeu!\n")
+    exit()
+    
+proxima_pergunta=input ("\nAperte Enter para próxima pergunta\n")
+
+cls()
+
+import time 
+string = "\n*Pergunta n° 3: Quem é a namorada do Mickey?\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n>>Escolha uma alternativa: <<\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+print ("\n1) Margarida\n")
+print ("\n2) Minnie\n")
+print ("\n3) A pequena sereia\n")
+print ("\n4) Olívia Palito\n")
+
+resposta3=int(input("\nQual é a resposta certa?\n\n"))
+
+if resposta3==2:
+    print ("\nParabéns, você acertou!!!\n")
+
+else:
+    print ("\nQue pena! Você perdeu!\n")
+    exit()
+    
+proxima_pergunta=input ("\nAperte Enter para próxima pergunta\n")
+
+cls()
+
+import time 
+string = "\n*Pergunta n° 4: Qual é o personagem do folclore brasileiro que tem uma perna só?\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n>>Escolha uma alternativa: <<\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+print ("\n1) Cuca\n")
+print ("\n2) Negrinho do pastoreio\n")
+print ("\n3) Boitatá\n")
+print ("\n4) Saci-Pererê\n")
+
+resposta4=int(input("\nQual é a resposta certa?\n\n"))
+
+if resposta4==4:
+    print ("\nParabéns, você acertou!!!\n")
+
+else:
+    print ("\nQue pena! Você perdeu!\n")
+    exit()
+    
+proxima_pergunta=input ("\nAperte Enter para próxima pergunta\n")
+
+cls()
+
+import time 
+string = "\n*Pergunta n° 5: Quem proclamou a república no Brasil em 1889?\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n>>Escolha uma alternativa: <<\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+print ("\n1) Duque de Caxias\n")
+print ("\n2) Marechal Rondon\n")
+print ("\n3) Dom Pedro II\n")
+print ("\n4) Marechal Deodoro\n")
+
+resposta5=int(input("\nQual é a resposta certa?\n\n"))
+
+if resposta5==4:
+    print ("\nParabéns, você acertou e passou para a próxima fase de perguntas!!!\n")
+
+else:
+    print ("\nQue pena! Você perdeu!\n")
+    exit()
+    
+proxima_pergunta=input ("\nAperte Enter para próxima pergunta\n")
+
+cls()
+
+import time 
+string = "\nSEGUNDA RODADA\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n*Pergunta nº 1: Qual é o país do tango?\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n>>Escolha uma alternativa: <<\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+print ("\n1) Uruguai\n")
+print ("\n2) Argentina\n")
+print ("\n3) Paraguai\n")
+print ("\n4) Espanha\n")
+
+resposta6=int(input("\nQual é a resposta certa?\n\n"))
+
+if resposta6==2:
+    print ("\nParabéns, você acertou!!!\n")
+
+else:
+    print ("\nQue pena! Você perdeu!\n")
+    exit()
+    
+proxima_pergunta=input ("\nAperte Enter para próxima pergunta\n")
+
+cls()
+
+import time 
+string = "\n*Pergunta nº 2: Que imperador pôs fogo em Roma?\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n>>Escolha uma alternativa: <<\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+print ("\n1) Trajano\n")
+print ("\n2) Nero\n")
+print ("\n3) Brutus\n")
+print ("\n4) Calígula\n")
+
+resposta7=int(input("\nQual é a resposta certa?\n\n"))
+
+if resposta7==2:
+    print ("\nParabéns, você acertou!!!\n")
+
+else:
+    print ("\nQue pena! Você perdeu!\n")
+    exit()
+    
+proxima_pergunta=input ("\nAperte Enter para próxima pergunta\n")
+
+cls()
+
+import time 
+string = "\n*Pergunta nº 3: A cidade de Pompéia, que foi soterrada por um vulcão fica em qual desses países?\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n>>Escolha uma alternativa: <<\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+print ("\n1) Japão\n")
+print ("\n2) México\n")
+print ("\n3) Itália\n")
+print ("\n4) Estados Unidos\n")
+
+resposta8=int(input("\nQual é a resposta certa?\n\n"))
+
+if resposta8==3:
+    print ("\nParabéns, você acertou!!!\n")
+
+else:
+    print ("\nQue pena! Você perdeu!\n")
+    exit()
+    
+proxima_pergunta=input ("\nAperte Enter para próxima pergunta\n")
+
+cls()
+
+import time 
+string = "\n*Pergunta nº 4: O que é um oboé?\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n>>Escolha uma alternativa: <<\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+print ("\n1) Vulcão\n")
+print ("\n2) Comida\n")
+print ("\n3) Instrumento Musical\n")
+print ("\n4) Tribo Indígena\n")
+
+resposta9=int(input("\nQual é a resposta certa?\n\n"))
+
+if resposta9==3:
+    print ("\nParabéns, você acertou!!!\n")
+
+else:
+    print ("\nQue pena! Você perdeu!\n")
+    exit()
+    
+proxima_pergunta=input ("\nAperte Enter para próxima pergunta\n")
+
+cls()
+
+import time 
+string = "\n*Pergunta n° 5: De quantos em quantos anos aparece o cometa Halley?\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n>>Escolha uma alternativa: <<\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+print ("\n1) 57 anos\n")
+print ("\n2) 67 anos\n")
+print ("\n3) 75 anos\n")
+print ("\n4) 79 anos\n")
+
+resposta10=int(input("\nQual é a resposta certa?\n\n"))
+
+if resposta10==3:
+    print ("\nParabéns, você acertou e passou para a próxima fase de perguntas!!!\n")
+
+else:
+    print ("\nQue pena! Você perdeu!\n")
+    exit()
+    
+proxima_pergunta=input ("\nAperte Enter para próxima pergunta\n")
+
+cls()
+
+import time 
+string = "\nTERCEIRA RODADA\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n*Pergunta nº 1: Qual desses astros de filmes de ação é belga?\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n>>Escolha uma alternativa: <<\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+print ("\n1) Arnold Schwarzenegger\n")
+print ("\n2) Sylvester Stallone\n")
+print ("\n3) Steven Seagal\n")
+print ("\n4) Jean Claude Van Damme\n")
+
+resposta11=int(input("\nQual é a resposta certa?\n\n"))
+
+if resposta11==4:
+    print ("\nParabéns, você acertou!!!\n")
+
+else:
+    print ("\nQue pena! Você perdeu!\n")
+    exit()
+    
+proxima_pergunta=input ("\nAperte Enter para próxima pergunta\n")
+
+cls()
+
+import time 
+string = "\n*Pergunta nº 2: Onde nasceu Van Gogh, o grande pintor impressionista?\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n>>Escolha uma alternativa: <<\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+print ("\n1) Polônia\n")
+print ("\n2) França\n")
+print ("\n3) Itália\n")
+print ("\n4) Holanda\n")
+
+resposta12=int(input("\nQual é a resposta certa?\n\n"))
+
+if resposta12==4:
+    print ("\nParabéns, você acertou!!!\n")
+
+else:
+    print ("\nQue pena! Você perdeu!\n")
+    exit()
+    
+proxima_pergunta=input ("\nAperte Enter para próxima pergunta\n")
+
+cls()
+
+import time 
+string = "\n*Pergunta nº 3: Brahma é o deus de que religião?\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n>>Escolha uma alternativa: <<\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+print ("\n1) Hinduísmo\n")
+print ("\n2) Xintoísmo\n")
+print ("\n3) Budismo\n")
+print ("\n4) Islamismo\n")
+
+resposta13=int(input("\nQual é a resposta certa?\n\n"))
+
+if resposta13==1:
+    print ("\nParabéns, você acertou!!!\n")
+
+else:
+    print ("\nQue pena! Você perdeu!\n")
+    exit()
+    
+proxima_pergunta=input ("\nAperte Enter para próxima pergunta\n")
+
+cls()
+
+import time 
+string = "\n*Pergunta nº 4: Qual o símbolo químico do radônio?\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n>>Escolha uma alternativa: <<\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+print ("\n1) Rr\n")
+print ("\n2) Rd\n")
+print ("\n3) Rn\n")
+print ("\n4) Ro\n")
+
+resposta14=int(input("\nQual é a resposta certa?\n\n"))
+
+if resposta14==3:
+    print ("\nParabéns, você acertou!!!\n")
+
+else:
+    print ("\nQue pena! Você perdeu!\n")
+    exit()
+    
+proxima_pergunta=input ("\nAperte Enter para próxima pergunta\n")
+
+cls()
+
+import time 
+string = "\n*Pergunta nº 5: A que país pertence a ilha de Terra Nova?\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n>>Escolha uma alternativa: <<\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+print ("\n1) Estados Unidos\n")
+print ("\n2) Dinamarca\n")
+print ("\n3) Canadá\n")
+print ("\n4) França\n")
+
+resposta15=int(input("\nQual é a resposta certa?\n\n"))
+
+if resposta15==3:
+    print ("\nParabéns, você acertou e passou para a rodada final, valendo 1 milhão de reais!!\n")
+
+else:
+    print ("\nQue pena! Você perdeu!\n")
+    exit()
+    
+proxima_pergunta=input ("\nAperte Enter para próxima pergunta\n")
+
+cls()
+
+import time 
+string = "\nÚLTIMA RODADA\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n*Pergunta de 1 milhão: Bill Clinton nasceu em que cidade?\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+import time 
+string = "\n>>Escolha uma alternativa: <<\n"
+for ch in string: 
+    time.sleep(0.01) 
+    print(ch, end='', flush=True)
+
+print ("\n1) Los Angeles\n")
+print ("\n2) Chicago\n")
+print ("\n3) Hope\n")
+print ("\n4) Dallas\n")
+
+resposta16=int(input("\nQual é a resposta certa?\n\n"))
+
+if resposta16==3:
+    print ("\nParabéns, você acertou e ganhou 1 milhão de reais!!!\n")
+    print ("\nObrigado por jogar!")
+
+else:
+    print ("\nQue pena! Você perdeu!\n")
+    exit()
